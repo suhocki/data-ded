86c596c79284c44e3252795d98370325bcbb6b55
awsme <suhockii@gmail.com>
2016-12-18 15:16:48 +0300

    Fixed post queries

--- scripts/api/postUser.js
+++ scripts/api/postUser.js
@@ -1,29 +1,25 @@
 'use strict';
 
-define({
-  request: function request(url, user) {
-    console.log(user);
-    return fetch(url, {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      // body: JSON.stringify({
-      //   AdminType: user.isAdmin,
-      //   Name: user.name,
-      //   Phone: user.phone,
-      //   Password: user.password,
-      // })
-      body: JSON.stringify({
-        name: '5262',
-        adminType: 'false',
-        phone: '+213',
-        password: 'sdas'
-      })
-    }).then(function (response) {
-      return response.json();
-    });
-  }
+define(function (require) {
+  return {
+    request: function request(url, user) {
+      var form = require('api/form');
+
+      return fetch(url, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: form.encode({
+          name: '5262',
+          adminType: 'false',
+          phone: '+213',
+          password: 'sdas'
+        })
+      }).then(function (response) {
+        return response.json();
+      });
+    }
+  };
 });
-//# sourceMappingURL=D:\client\api\postUser.js.map
+//# sourceMappingURL=C:\Users\collapse\Desktop\data-ded-master\scripts\api\postUser.js.map
